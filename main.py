086122d0919ad8bc16090f75ccfdbf8eaa1bfe31
Kamil Stróżyk <strozykkamil@gmail.com>
2020-03-09 22:35:20 +0100

    Add dark theme

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+import ttkthemes
+import tkinter
 from tkinter import *
-
+from tkinter import ttk
+from ttkthemes import ThemedStyle
 
 class App:
     def __init__(self):
         self.window = Tk()
         self.window.title("Tomograph")
+        self.window.config(bg='#FFFAFA')
         self.window.geometry('1280x720')
-        
-        self.lbl = Label(self.window, text="Hello")
-        self.lbl.grid(column=0, row=0)
-        self.txt = Entry(self.window,width=10)
-        self.txt.grid(column=1, row=0)
-        self.btn = Button(self.window, text="Click Me", command=self.clicked)
-        self.btn.grid(column=2, row=0)
+
+        self.style = ThemedStyle(self.window)
+        self.style.set_theme("equilux")
+
+        self.main_frame = ttk.Frame(self.window, width = 1280)
+        self.main_frame.grid(column=0, row=0)
+        self.main_frame.pack(anchor=W, fill=Y, expand=False, side=LEFT)  # <----
+
+        self.lbl = ttk.Label(self.main_frame, text="Hello")
+        # self.lbl.grid(column=0, row=0)
+        self.txt = ttk.Entry(self.main_frame,width=10)
+        # self.txt.grid(column=1, row=0)
+        self.btn = ttk.Button(self.main_frame, text="Click Me", command=self.clicked)
+        # self.btn.grid(column=2, row=0)
         self.window.mainloop()
 
     def clicked(self):
