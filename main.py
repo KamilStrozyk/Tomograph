c401a1fb746504bbc8629b7355de62601f34654a
Kamil Stróżyk <strozykkamil@gmail.com>
2020-03-09 22:07:29 +0100

    Add simple test UI

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from tkinter import *
 
-window = Tk()
 
-window.title("Tomograph")
+class App:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Tomograph")
+        self.window.geometry('1280x720')
+        
+        self.lbl = Label(self.window, text="Hello")
+        self.lbl.grid(column=0, row=0)
+        self.txt = Entry(self.window,width=10)
+        self.txt.grid(column=1, row=0)
+        self.btn = Button(self.window, text="Click Me", command=self.clicked)
+        self.btn.grid(column=2, row=0)
+        self.window.mainloop()
 
-window.mainloop()
+    def clicked(self):
+        self.lbl.configure(text="Button was clicked !!")
+
+if __name__ == '__main__':
+    app = App()
+
+
