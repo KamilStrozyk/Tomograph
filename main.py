c193890a256a2b215ad182a7193644e46341cc3d
Kamil Stróżyk <strozykkamil@gmail.com>
2020-03-11 12:36:41 +0100

    filedialog works

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import ttkthemes
 import tkinter
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 from ttkthemes import ThemedStyle
 import pygubu
 
+def addFile(self):
+    print("XD")
+    pass
 
-class App:
+class App(pygubu.TkApplication):
     def __init__(self):
         self.builder = builder = pygubu.Builder()
         builder.add_from_file('Interface.ui')
@@ -14,14 +18,26 @@ class App:
         self.window = builder.get_object('main_window')
         self.window.title("Tomograph")
         self.window.geometry('700x740')
+        self._create_ui()
 
         self.style = ThemedStyle(self.window)
         self.style.set_theme("winnative")
         self.window.mainloop()
 
     def addFile(self):
-        print("XD")
+        self.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
         pass
+
+    def run(self):
+        pass
+
+    def _create_ui(self):
+        callbacks = {
+            'addFile': self.addFile,
+            'run': self.run,
+            }
+        self.builder.connect_callbacks(callbacks)
+        
 
 
 if __name__ == '__main__':
